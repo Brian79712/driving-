c7f3ca5d4aa4b8df127f67811bfa90d3e9157c79
Brian Lin <yoyoboy323@gmail.com>
2019-08-21 10:22:07 -0400

    add driving file

---
+++ driving.py
@@ -0,0 +1,15 @@
+country = input('你是哪一國人: ')
+age = input('請問年齡幾歲: ')
+age = int('age')
+if country == ('台灣'):
+	if age >= 18:
+		print('你可以考駕照')
+	else:
+		print('你不能考駕照')
+if country == ('美國')
+	if age >= 16:
+		print('你可以考駕照')
+	else:
+		print('你不能考駕照')
+else:
+	print('只能輸入 台灣/美國')
